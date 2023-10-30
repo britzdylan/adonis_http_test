1010d9c90d9c5fc21dfefcaa87010053e9144d6e
Dylan Britz <britzdylan@gmail.com>
2023-10-30 22:45:36 +0100

    http tests

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,30 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
+Route.get('/', async (ctx) => {
+  return ctx.view.render('welcome', { data: ctx })
+})
+
+Route.get('/request', async ({ view, request }) => {
+  return view.render('welcome', { data: request.toJSON() })
+})
+
+Route.get('/response', async ({ view, response }) => {
+  return view.render('welcome', { data: response })
+})
+
+Route.get('/session', async ({ view, session }) => {
+  return view.render('welcome', { data: session.sessionId })
+})
+
+Route.get('/ip', async ({ view, request }) => {
+  return view.render('welcome', { data: request.ip() })
+})
+
+Route.get('/user-agent', async ({ view, request }) => {
+  return view.render('welcome', { data: request.header('user-agent') })
+})
+
+Route.get('/host', async ({ view, request }) => {
+  return view.render('welcome', { data: request.hostname() })
 })
